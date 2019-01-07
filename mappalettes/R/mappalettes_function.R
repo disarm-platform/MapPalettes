eb062a5dfcd372c06ab1cc70b1f2b101d2fa7be8
Hugh Sturrock <hughsturrock@gmail.com>
2019-01-07 21:22:30 +0200

    First commit

---
+++ mappalettes/R/mappalettes_function.R
@@ -0,0 +1,20 @@
+#' The map palette function
+#'
+#' This function allows you to easily access beautiful palettes designed for maps.
+#' @param name The name of the palette 
+#' @param n The number of colors in the palette
+#' @keywords cats
+#' @export
+#' @examples map_palette("hugh_div)
+
+map_palette <- function(name,
+                        n=5){
+  
+  if(name=="hugh_div"){
+    palette <- c("#1D3141","#096168","#209478","#75C56E","#E2EE5E")
+  }
+    
+    pal <- colorRampPalette(palette)
+    pal(n)
+}
+
